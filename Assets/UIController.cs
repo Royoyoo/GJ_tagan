1df736196827ddc182c8cf8dffd3033729edf124
Purebyte <Purebyte@mail.ru>
2017-05-13 18:44:30 +0300

    Effects

---
+++ Assets/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+	public Text scoreText;
+	public Text mudText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "Очки:" + GameController.instance.playerScript.score.ToString ();
+		mudText.text = "Грязь:" + GameController.instance.playerScript.mud.ToString ();
+	}
+}
